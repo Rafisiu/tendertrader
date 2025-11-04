653ed2ce72b21500eabac7be053590edb39db1c6
Rafisiu <117052612+Rafisiu@users.noreply.github.com>
2025-11-04 14:57:13 +0700

    Update Supabase URL and publishable key

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://tltvtmsomgcudgzfogzg.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsdHZ0bXNvbWdjdWRnemZvZ3pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3MDcyMjYsImV4cCI6MjA1OTI4MzIyNn0.VR0JRx11ezxIU1yOb2jRao7XjIzuHubmW7wqqiCRqvA";
+const SUPABASE_URL = "https://jrhxbiiavhkuxjtfvivb.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyaHhiaWlhdmhrdXhqdGZ2aXZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNDE4ODUsImV4cCI6MjA3NzgxNzg4NX0.qtLF65mrjKhhcEsT1r6S9L79eSoPEncGRny9mkauV1Q";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
